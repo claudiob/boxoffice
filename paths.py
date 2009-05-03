25337ff54c6bfb67f825ae06ed74ccb8f5d8338f
claudiob <claudiob@gmail.com>
2009-05-03 17:41:54 +0200

    Multiple movies can be compared on the same graph.

--- paths.py
+++ paths.py
@@ -17,12 +17,15 @@ from download import *
 
 __author__ = "Claudio Baccigalupo"
 
-def plot(movie_id, full_week=False, use_cumes=False): 
-    values = parse(movie_id, full_week, use_cumes)
+# chdl=titanic|test
+# chco=4D89F9,FF0000
+# chd=t:28638131,35455673,33315278|8638131,5455673,3315278
+
+def plot(chd, chdl, chco, chdmax): 
     chart_url = "http://chart.apis.google.com/chart?"
     chart_url = chart_url + "chs=1000x300" # Width
     chart_url = chart_url + "&chtt=U.S.+Weekend+Gross&chts=FF0000,20" # Title
-    chart_url = chart_url + "&chdl=" + movie_id # Legend
+    chart_url = chart_url + "&chdl=" + chdl # Legend
     chart_url = chart_url + "&chxt=x,y" # Axis type
     chart_url = chart_url + "&chxl=0:|1||||5|||||10|||||15|||||20|||||" + \
                             "25|||||30|||||35|||||40|||||45|||||50" + \
@@ -30,19 +33,13 @@ def plot(movie_id, full_week=False, use_cumes=False):
     chart_url = chart_url + "&chxr=0,1,50|1,0,1000" # Axis range
     chart_url = chart_url + "&cht=bvg" # Bar charts
     chart_url = chart_url + "&chbh=a" # Bar charts width
-    chart_url = chart_url + "&chco=4D89F9" # Colors
-    # If relative:
-    chart_url = chart_url + "&chds=0," + values[-1] # Data scaling
-    # Else:
-    #chart_url = chart_url + "&chds=0,700000000" # Data scaling
-    # Data
-    chart_url = chart_url + "&chd=t:"
-    chart_url = chart_url + ",".join(values)
+    chart_url = chart_url + "&chco=" + chco # Colors
+    chart_url = chart_url + "&chds=0," + chdmax # Data scaling
+    chart_url = chart_url + "&chd=t:" + chd
     webbrowser.open(chart_url)
-    return values
+    return chart_url
     
     
-
 def parse(movie_id, full_week=False, use_cumes=False): 
     '''Return list of friends and, if not only_id, page count and is artist.'''
 
@@ -52,7 +49,7 @@ def parse(movie_id, full_week=False, use_cumes=False):
             daily_pattern = r'<font color="#000080">\$([0-9,]*?)</font>'
             grosses =  re.findall(daily_pattern, page)
         else:
-            weekend_patterns = r'<td align="center"><font size="2">([0-9]*?)</font></td><td align="right"><font size="2">\$([0-9,]*?)</font></td>'
+            weekend_patterns = r'<td align="center"><font size="2">([0-9\-]*?)</font></td><td align="right"><font size="2">\$([0-9,]*?)</font></td>'
             grosses =  re.findall(weekend_patterns, page)
             grosses = [g[1] for g in grosses]
         return [g.replace(",","") for g in grosses]
@@ -87,10 +84,10 @@ def main(argv=None):
 
     def usage():
         # Add an interactive version to add parameters one by one
-        print ("Usage: %s movietitle" % argv[0])
+        print ("Usage: %s movietitles" % argv[0])
         print ("   options:")
         print ("   -h [--help] print this usage statement")
-        print ("   -w [--week] print full week (not weekend")
+        print ("   -w [--week] print full week (not weekend)")
         print ("   -c [--cumes] print cumulatives")
         return
 
@@ -119,10 +116,10 @@ def main(argv=None):
         ###### 3. Process args ######
         if len(args) < 1:
             raise Usage("You did not specify a movie ID")
-        elif len(args) > 1:
-            raise Usage("You specified more than one movie ID")
+        #elif len(args) > 1:
+        #    raise Usage("You specified more than one movie ID")
         else:
-            movie_title = args[0]
+            movie_titles = args
         ###### 4. Enable logging ######
         logging_config["format"] = '%(asctime)s %(levelname)-8s %(message)s'
         logging_config["datefmt"] = '%Y/%M/%D %H:%M:%S'
@@ -131,7 +128,21 @@ def main(argv=None):
             logging_config["filemode"] = "w"
         logging.basicConfig(**logging_config)
         ###### 5. Plot data ######
-        result = plot(movie_title, full_week, use_cumes)
+        chd = []
+        chdmax = []
+        for movie_title in movie_titles:
+            parsed = parse(movie_title, full_week, use_cumes)
+            chdmax.append(max(map(int, parsed)))
+            chd.append(",".join(parsed))
+        chd = "|".join(chd)
+        chdl = "|".join(movie_titles)
+        chdmax = max(chdmax)
+        colors = ["AA0000", "AAAA00", "00AA00", "00AAAA", "0000AA", "AA00AA"]
+        chco = ",".join(colors[0:len(movie_titles)])
+        result = plot(chd, chdl, chco, str(chdmax))
+        
+
+
         return result
     ###### Manage errors ######
     except Usage, err:
